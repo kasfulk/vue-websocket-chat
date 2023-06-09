cf5af8831b84d9ba0793678a96dff4a53feeb852
Kasjful Kurniawan <kasful.bpp@gmail.com>
2023-06-09 08:32:55 +0800

    refactor: create from zero

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,13 +3,3 @@
     <h1 class="text-sm">This is an about page</h1>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
